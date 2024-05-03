8b7e323ffffefba7009af1b1261ec76a9ed5768c
t-nagatani@u22pa <tomoki329@gmail.com>
2024-05-03 15:47:50 +0900

    add docker_push.sh

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# コンテナをpushするためのスクリプト。
+# docker loginは手動でやってから、これを実行してほしい。
+
+set -e
+
 source scripts/docker_util.sh
+$docker_compose --profile runtime_base push
+$docker_compose --profile runtime      push
