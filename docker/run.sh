884f8d7e2f4db8a134a364e7c515f1d55d8e6a11
t-nagatani <tomoki329@gmail.com>
2024-05-12 13:08:04 +0900

    update xhost setting

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,10 @@ if [ ! -e docker/home/.user_config.bash ]; then
     cp docker/.default_do_not_edit/user_config.bash docker/home/.user_config.bash
 fi
 
+if [ -z "$container_list" ]; then # コンテナ起動してなければ実行その１
+    xhost + # ディスプレイ表示用、これをexport DISPLAYの前に実行する必要がある。
+fi
+
 source docker/internal/docker_util.sh
 
 export HOST_UID=`id -u`
@@ -21,8 +25,7 @@ fi
 
 container_list=`$docker_compose ps -q`
 
-if [ -z "$container_list" ]; then # コンテナ起動してなければ起動
-    xhost + # ディスプレイ表示用
+if [ -z "$container_list" ]; then # コンテナ起動してなければ起動その２
     $docker_compose --profile runtime_base up --no-start --no-recreate
     $docker_compose --profile runtime_base down
 
