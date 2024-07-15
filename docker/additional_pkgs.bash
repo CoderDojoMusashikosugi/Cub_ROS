ec5f6741f597b3d9be2df8499bb4eef858ad00ca
t-nagatani@u22wkwsl <tomoki329@gmail.com>
2024-07-15 19:30:44 +0900

    sim for turtlebot3 on x86_64

--- docker/additional_pkgs.bash
+++ docker/additional_pkgs.bash
@@ -4,4 +4,5 @@ echo "Package: *
 Pin: release o=LP-PPA-openrobotics-gazebo11-non-amd64
 Pin-Priority: 499" > /etc/apt/preferences.d/gazebo11-non-amd64
 apt update
-# apt -y install
+apt -y install ros-humble-turtlebot3-navigation2
+sed -i -e "s/differential/nav2_amcl::DifferentialMotionModel/g" /opt/ros/humble/share/turtlebot3_navigation2/param/burger.yaml
