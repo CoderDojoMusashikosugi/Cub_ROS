7314a9ccc28164e08a2c1e0698fd6d1a0538a42f
t-nagatani <tomoki329@gmail.com>
2024-08-07 01:20:32 +0900

    revert additinal_pkgs

--- docker/additional_pkgs.bash
+++ docker/additional_pkgs.bash
@@ -6,5 +6,5 @@ Pin-Priority: 499" > /etc/apt/preferences.d/gazebo11-non-amd64
 apt update
 # apt -y install
 apt -y install libpcap0.8 ros-humble-librealsense2 ros-humble-realsense2-camera ros-humble-bno055 ros-humble-nmea-navsat-driver ros-humble-rosbag2-storage-mcap
-apt -y install ros-humble-turtlebot3-navigation2 ros-humble-turtlebot3-msgs ros-humble-gazebo-ros ros-humble-camera-info-manager ros-humble-xacro ros-humble-turtlebot3-description ros-humble-ros-ign-gazebo ros-humble-ros-ign-bridge ros-humble-controller-manager ros-humble-ign-ros2-control ros-humble-joint-state-broadcaster ros-humble-diff-drive-controller
+apt -y install ros-humble-turtlebot3-navigation2
 sed -i -e "s/differential/nav2_amcl::DifferentialMotionModel/g" /opt/ros/humble/share/turtlebot3_navigation2/param/burger.yaml
