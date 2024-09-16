d2bb91b1d88b299c4e0a9386b10e610a977ba3f3
t-nagatani@raspi5 <tomoki329@gmail.com>
2024-09-16 16:57:08 +0900

    select cub2 or mcub using environment variable

---
+++ ros/cub_bringup/launch/target_mcub.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='wheel_odometry',
+            executable='wheel_odometry_node',
+        ),
+    ])
