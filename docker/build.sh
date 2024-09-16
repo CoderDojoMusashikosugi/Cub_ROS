33016165a21ffe41ddc10e815b8fe71af735b723
t-nagatani <tomoki329@gmail.com>
2024-09-16 19:24:51 +0900

    bug fix for update ver sequence

--- docker/build.sh
+++ docker/build.sh
@@ -5,11 +5,12 @@ set -e
 
 ./stop.sh
 
+source docker/internal/docker_util.sh
+
 if [ ${1:-update} != "stay" ]; then
     export VER=`date "+%Y%m%d_%H%M%S"`
 fi
 
-source docker/internal/docker_util.sh
 $docker_compose --profile runtime_base up --no-start # buildじゃなくてupなのは、一旦pull出来ないか確認するため
 $docker_compose --profile runtime_base down # buildじゃないのでコンテナ出来ちゃうから、終わったら落としておく
 
