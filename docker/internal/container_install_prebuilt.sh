98c3a0b25177f0c88492cbad61fdb5983a87acd7
t-nagatani <tomoki329@gmail.com>
2024-09-16 19:25:28 +0900

    enable prebuilt packages and build micro_ros

---
+++ docker/internal/container_install_prebuilt.sh
@@ -0,0 +1,12 @@
+export DEBIAN_FRONTEND=noninteractive
+apt update
+
+mkdir -p /prebuilt_ws/src && cd /prebuilt_ws/src
+git clone --depth 1 -b humble https://github.com/micro-ROS/micro_ros_setup.git
+cd /prebuilt_ws && . /opt/ros/humble/setup.sh && rosdep init && rosdep update --rosdistro=humble
+rosdep install --from-paths src --ignore-src -y
+colcon build
+. /prebuilt_ws/install/local_setup.sh
+ros2 run micro_ros_setup create_agent_ws.sh
+ros2 run micro_ros_setup build_agent.sh
+rm -rf log/ build/ src/
