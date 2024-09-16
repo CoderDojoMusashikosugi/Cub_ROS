22074283d38808209c6861ecf3447e07107cf362
t-nagatani <tomoki329@gmail.com>
2024-09-16 19:29:57 +0900

    add bno055 udev rule

--- scripts/install_host_settings.sh
+++ scripts/install_host_settings.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo "This script setups host settings."
+echo "run as sudo"
+echo "for example: sudo ./mcub_host_settings.sh"
+echo 'KERNEL=="ttyUSB*",  ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="ttyBNO055"' > /etc/udev/rules.d/99-bno055.rules
+echo "reboot to apply"
