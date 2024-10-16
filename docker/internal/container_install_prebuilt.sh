250970b232e6805499081659a657c10c8bfd76d4
t-nagatani <tomoki329@gmail.com>
2024-10-17 00:00:56 +0900

    add gazebo_ros_pkgs and turtlebot3_simulations

--- docker/internal/container_install_prebuilt.sh
+++ docker/internal/container_install_prebuilt.sh
@@ -3,9 +3,10 @@ apt update
 
 mkdir -p /prebuilt_ws/src && cd /prebuilt_ws/src
 git clone --depth 1 -b humble https://github.com/micro-ROS/micro_ros_setup.git
+git clone --depth 1 -b 3.7.0 https://github.com/ros-simulation/gazebo_ros_pkgs.git
 cd /prebuilt_ws && . /opt/ros/humble/setup.sh && rosdep init && rosdep update --rosdistro=humble
 rosdep install --from-paths src --ignore-src -y
-colcon build
+MAKEFLAGS="-j 1" colcon build
 . /prebuilt_ws/install/local_setup.sh
 ros2 run micro_ros_setup create_agent_ws.sh
 ros2 run micro_ros_setup build_agent.sh
