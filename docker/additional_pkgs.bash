e9af45e55cebc926c07076a4e488237272f2796a
miyao323 <miyao03023@gmail.com>
2024-10-17 22:36:49 +0900

    improve stavility micro-ros task

--- docker/additional_pkgs.bash
+++ docker/additional_pkgs.bash
@@ -1,4 +1,5 @@
 # ここにインストールしたいパッケージを書く
 apt update
+apt -y install ros-humble-rosbag2-storage-mcap
 # apt -y install
 apt -y install libpcap0.8 ros-humble-librealsense2 ros-humble-realsense2-camera ros-humble-bno055 ros-humble-nmea-navsat-driver ros-humble-rosbag2-storage-mcap
