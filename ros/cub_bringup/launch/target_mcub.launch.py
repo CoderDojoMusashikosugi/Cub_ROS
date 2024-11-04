1ed9cd9c2eedc9a92a6cf01f6cc940a4addbc7d0
t-nagatani@raspi5 <tomoki329@gmail.com>
2024-11-04 23:24:21 +0900

    enable autonomous navigation for mcub

--- ros/cub_bringup/launch/target_mcub.launch.py
+++ ros/cub_bringup/launch/target_mcub.launch.py
@@ -1,13 +1,56 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+import os
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'mcub.urdf'
+    urdf = os.path.join(
+        get_package_share_directory('cub_description'),
+        'urdf',
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
     return LaunchDescription([
         Node(
             package='wheel_odometry',
             executable='wheel_odometry_node',
+        ),
+        Node(
+            package='cub_bringup',
+            executable='odom_to_tf',
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf]),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(FindPackageShare('sllidar_ros2').find('sllidar_ros2'), 'launch', 'sllidar_c1_launch.py')
+            ]),
+            launch_arguments={
+                'serial_port': "/dev/ttySLC1",
+                'frame_id': "SLC1_link",
+            }.items()
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', PathJoinSubstitution([FindPackageShare('cub_visualization'), 'config', 'mcub.rviz'])],
+            output='screen',
         ),
     ])
