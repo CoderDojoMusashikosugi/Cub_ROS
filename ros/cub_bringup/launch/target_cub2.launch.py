e1a951975e82140ba43c174e08df5355b1e79742
Jetson <musashikosugi@ubuntu.local>
2024-12-07 08:01:29 +0900

    fix real velodyne node

--- ros/cub_bringup/launch/target_cub2.launch.py
+++ ros/cub_bringup/launch/target_cub2.launch.py
@@ -29,37 +29,6 @@ def generate_launch_description():
             name='wheel_odometry_node',
             output='screen'
         ),
-        # correction point cloud tilt
-        Node(
-            package='cub_bringup',
-            executable='tilt_correction_node',
-            name='tilt_correction_node',
-            parameters=[{
-                'tilt_angle_degrees': 2.07
-            }],
-        ),
-
-        # pointcloud to laser scan
-        Node(
-            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', '/velodyne_points_corrected'),
-                        ('scan', '/scan')],
-            parameters=[{
-                'target_frame': '',
-                'transform_tolerance': 0.01,
-                'min_height': -0.35,
-                'max_height': 1.5,
-                'angle_min': -3.14,  # -M_PI
-                'angle_max': 3.14,  # M_PI
-                'angle_increment': 0.0087,  # M_PI/360.0
-                'scan_time': 0.3333,
-                'range_min': 0.1,
-                'range_max': 100.0,
-                'use_inf': True,
-                'inf_epsilon': 1.0
-            }],
-            name='pointcloud_to_laserscan'
-        ),
 
         # RVizの起動
         IncludeLaunchDescription(
