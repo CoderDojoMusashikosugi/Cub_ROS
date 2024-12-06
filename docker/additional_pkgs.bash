5e50c3eff781d491498b990787cc1c791ab058d6
Jetson <musashikosugi@ubuntu.local>
2024-12-07 08:01:29 +0900

    fix real velodyne node

--- docker/additional_pkgs.bash
+++ docker/additional_pkgs.bash
@@ -2,4 +2,4 @@
 apt update
 # apt -y install
 apt -y install libpcap0.8 ros-humble-bno055 ros-humble-nmea-navsat-driver ros-humble-rosbag2-storage-mcap ros-humble-rqt-tf-tree qtbase5-dev libqt5svg5-dev libzmq3-dev libdw-dev \
-    ros-humble-pcl-ros ros-humble-pcl-conversions ros-humble-nav2-map-server 
+    ros-humble-pcl-ros ros-humble-pcl-conversions ros-humble-nav2-map-server libeigen3-dev
