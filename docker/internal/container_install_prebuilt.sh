06785e9c1907769008039f1a00ac1c3b2d049a6b
t-nagatani <tomoki329@gmail.com>
2025-06-29 22:36:12 +0900

    add setup.bash for prebuilt

--- docker/internal/container_install_prebuilt.sh
+++ docker/internal/container_install_prebuilt.sh
@@ -1,6 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update
 
+source /opt/ros/humble/setup.bash
 mkdir -p /prebuilt_ws/src && cd /prebuilt_ws/src
 git clone --depth 1 -b humble https://github.com/micro-ROS/micro_ros_setup.git
 cd /prebuilt_ws && . /opt/ros/humble/setup.sh && rosdep init && rosdep update --rosdistro=humble
