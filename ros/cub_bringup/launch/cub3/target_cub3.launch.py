84483bf4fc7a0ae0678d11e9d033ae86dc21e6d8
nobu323 <miyao03023@gmail.com>
2025-07-05 22:58:45 +0900

    add hdl_localizer

--- ros/cub_bringup/launch/cub3/target_cub3.launch.py
+++ ros/cub_bringup/launch/cub3/target_cub3.launch.py
@@ -10,31 +10,43 @@ from launch_ros.actions import PushRosNamespace
 import os
 
 def generate_launch_description():
-    # sllidar_ros2パッケージの共有ディレクトリを取得
-    state_launch_file_dir = os.path.join(
-        get_package_share_directory('cub_description'),
-        'launch',
-        'display.launch.py'
-    )
-
-    # state_publisherの起動
-    state_publisher_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(state_launch_file_dir))
-    
     # Launchファイルの返り値
     return LaunchDescription([
         # wheel_odometryノード
+        # Node(
+        #     package='wheel_odometry',
+        #     executable='wheel_odometry_node',
+        #     name='wheel_odometry_node',
+        #     output='screen'
+        # ),
+
+        # pointcloud to laser scan
         Node(
-            package='wheel_odometry',
-            executable='wheel_odometry_node',
-            name='wheel_odometry_node',
-            output='screen'
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/velodyne_points'),
+                        ('scan', '/scan')],
+            parameters=[{
+                'target_frame': '',
+                'transform_tolerance': 0.01,
+                'min_height': -0.35,
+                'max_height': 1.5,
+                'angle_min': -3.14,  # -M_PI
+                'angle_max': 3.14,  # M_PI
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.1,
+                'range_max': 100.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
         ),
 
         # 2Dのマッピングやナビゲーションを実行する際はこれを有効化、3Dでは無効化
-        # Node(
-        #     package='cub_bringup',
-        #     executable='odom_to_tf',
-        # ),
+        Node(
+            package='cub_bringup',
+            executable='odom_to_tf',
+        ),
 
         # RVizの起動
         IncludeLaunchDescription(
@@ -45,5 +57,12 @@ def generate_launch_description():
             )
         ),
 
-        state_publisher_launch,
+        # state_publisherの起動
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution(
+                    [FindPackageShare("cub_description"), "launch", "display.launch.py"]
+                )
+            )
+        ),
     ])
