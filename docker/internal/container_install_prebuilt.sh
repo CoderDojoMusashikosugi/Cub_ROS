4900cfc4ff22d927d29a97eea05fd320b1c81efc
Nyanziba <126231924+Nyanziba@users.noreply.github.com>
2025-08-02 11:41:34 +0900

    update docker file

--- docker/internal/container_install_prebuilt.sh
+++ docker/internal/container_install_prebuilt.sh
@@ -1,6 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt-get install -y  curl gpg 
  
+
 source /opt/ros/jazzy/setup.bash
 mkdir -p /prebuilt_ws/src && cd /prebuilt_ws/src
 git clone --depth 1 -b jazzy https://github.com/micro-ROS/micro_ros_setup.git
