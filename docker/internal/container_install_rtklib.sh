3ee18e0ec0a86f9fd024a36ff0523766563b50a3
t-nagatani <tomoki329@gmail.com>
2025-09-09 22:49:10 +0900

    add rtklib and delete docker outside of docker

---
+++ docker/internal/container_install_rtklib.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update && apt install -y git build-essential
+cd /
+git clone -b rtklib_2.4.3 https://github.com/tomojitakasu/RTKLIB.git
+cd /RTKLIB/lib/iers/gcc/
+make
+cd /RTKLIB/app/consapp
+make
+make install
