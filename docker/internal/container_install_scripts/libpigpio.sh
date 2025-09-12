9224b7fa9df9d69456b3ea0336dca90f0f1c9cde
t-nagatani <tomoki329@gmail.com>
2025-09-12 23:52:41 +0900

    bug fix for libpigpio installation

--- docker/internal/container_install_scripts/libpigpio.sh
+++ docker/internal/container_install_scripts/libpigpio.sh
@@ -1,4 +1,9 @@
 apt update && apt -y install wget
 wget -P /etc/apt/keyrings/ http://archive.raspberrypi.com/debian/raspberrypi.gpg.key
 echo "deb [signed-by=/etc/apt/keyrings/raspberrypi.gpg.key] http://archive.raspberrypi.com/debian/ bookworm main" > /etc/apt/sources.list.d/raspi.list
+sudo tee /etc/apt/preferences.d/90-raspberrypi-pin > /dev/null <<'EOF'
+Package: *
+Pin: origin "archive.raspberrypi.com"
+Pin-Priority: 100
+EOF
 apt update && apt install -y libpigpio-dev
